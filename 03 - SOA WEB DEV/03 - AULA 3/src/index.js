877c47b12c3a7c9622f220a4cd82a1179fd6a810
Rafael Carvalho <rafaelcarvalho@Rafaels-MacBook-Air.local>
2025-02-26 09:29:38 -0300

    finalizando código busca api cep aula 3

--- 03 - SOA WEB DEV/03 - AULA 3/src/index.js
+++ 03 - SOA WEB DEV/03 - AULA 3/src/index.js
@@ -1,20 +1,37 @@
-const express = require("express");
+const express = require('express');
 
-const axios = require("axios")
-
+const axios = require('axios');
 
 const app = express();
 
 const PORT = 3000;
 
-//endpoint para buscar o endereço pelo CEP
 
 app.get('/cep/:cep', async (req, res) => {
-    const {cep} = req.params;
 
+    const { cep } = req.params;
 
-    try{
-        const response = await axios.get('https://viacep.com.br/ws/${cep}/json/')
+    try {
+        const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
 
+        const endereco = response.data;
+
+        if (endereco.erro) {
+            return res.status(404).json({ mensagem: 'CEP não encontrado' });
+        }
+
+        res.json({
+            cep: endereco.cep,
+            logradouro: endereco.logradouro,
+            bairro: endereco.bairro,
+            cidade: endereco.localidade,
+            estado: endereco.uf
+        });
+    } catch (error) {
+        res.status(500).json({ mensagem: 'Erro ao consultar o cep' });
     }
-})
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
